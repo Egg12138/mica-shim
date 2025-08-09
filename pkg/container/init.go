6321801bcac6c036b6666ae4fd5fbecda0844f47
egg12138 <zhejunxiao@126.com>
2025-08-09 16:31:01 +0800

    remove Baremetal and Jailhouse from the placeholder pedtype list; now it is determined that only Xen is capable of reaching out goals

--- pkg/container/init.go
+++ pkg/container/init.go
@@ -5,8 +5,8 @@ var HostMaxCPU int
 
 // Some crutial information should be shared between shim instances!
 func init() {
-
-	HostPedestalType = hostPed()
-
-	HostMaxCPU = availableMaxCPU()
+	if HostPedestalType == Unsupported {
+		HostPedestalType = hostPed()
+		HostMaxCPU = availableMaxCPU()
+	}
 }
